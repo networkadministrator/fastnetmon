ab48212d328496687d0fff8f409d01ceb12d3891
networkadministrator <mustafa@mustafakaravus.com>
2018-11-06 13:39:36 +0200

    degisik
    degisiklik

--- src/fastnetmon.cpp
+++ src/fastnetmon.cpp
@@ -2434,8 +2434,8 @@ void traffic_draw_program() {
         sorter = PACKETS;
     }
 
-    output_buffer << "Netbudur"
-                  << " www.netbudur.com"
+    output_buffer << "Codit Teknoloji"
+                  << " www.codit.com.tr"
                   << "\n"
                   << "IPs ordered by: " << sort_parameter << "\n";
 
